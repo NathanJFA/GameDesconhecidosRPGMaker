37887c127620b731783644a782abe5d75e84811a
DenilsonSilvaMonteiroFilho <49108665+DenilsonSilvaMonteiroFilho@users.noreply.github.com>
2021-06-06 20:17:44 -0300

    Implementado a interface Item

--- rpgmaker/src/main/java/Entities/Item.java
+++ rpgmaker/src/main/java/Entities/Item.java
@@ -1,5 +1,8 @@
 package Entities;
 
 public interface Item {
-    
+    public String getNome();
+    public boolean getEquipavel();
+    public void setEquipavel(boolean newTipoItem);
+    public int getHabilidade();
 }
