e4ed8ef2450cdb51157bdd52deac20650cd2fc84
DenilsonSilvaMonteiroFilho <49108665+DenilsonSilvaMonteiroFilho@users.noreply.github.com>
2021-06-06 20:24:14 -0300

    Item

--- rpgmaker/src/main/java/Entities/Item.java
+++ rpgmaker/src/main/java/Entities/Item.java
@@ -4,5 +4,6 @@ public interface Item {
     public String getNome();
     public boolean getEquipavel();
     public void setEquipavel(boolean newTipoItem);
-    public int getHabilidade();
+    public int getHabilidade();/*Tem que pensar de uma maneira de implemntar esse
+    metodo de forma que ele so retorne a habilidade que ele vai alterar*/
 }
