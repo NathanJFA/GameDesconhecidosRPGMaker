01205293ca83248b437f3b78913954983f75e022
nathan <nathan.farias@dcx.ufpb.br>
2021-06-14 22:15:24 -0300

    Exemplo do esqueleto do controller..

---
+++ rpgmaker/src/main/java/Telas/FramePrincipalController.java
@@ -0,0 +1,34 @@
+package Telas;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+
+public class FramePrincipalController {
+
+    @FXML
+    private ToggleButton btCriarAventura;
+
+    @FXML
+    private ToggleButton btBibliotecaJogos;
+
+    @FXML
+    private ToggleButton btExit;
+
+    @FXML
+    void createAdventure(ActionEvent event) {
+        //TODO
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+        //TODO
+    }
+
+    @FXML
+    void librarieGames(ActionEvent event) {
+        //TODO
+    }
+
+}
+
--- rpgmaker/src/main/java/desconhecidos/game/Main.java
+++ rpgmaker/src/main/java/desconhecidos/game/Main.java
@@ -7,18 +7,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("teste.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../../Telas/TelaPrincipal.fxml"));
         Parent root = fxmlLoader.load();
         Scene tela = new Scene(root);
-
 
         primaryStage.setScene(tela);
         primaryStage.show();
