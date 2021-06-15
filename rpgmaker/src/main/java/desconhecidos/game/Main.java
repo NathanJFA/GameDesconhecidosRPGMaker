049ddcdf1d4b9d60316bb7b6cfc936284c44ffda
nathan <nathan.farias@dcx.ufpb.br>
2021-06-14 21:06:35 -0300

    linux bug 9vamente

--- rpgmaker/src/main/java/desconhecidos/game/Main.java
+++ rpgmaker/src/main/java/desconhecidos/game/Main.java
@@ -1,18 +1,10 @@
 package desconhecidos.game;
 
-import com.truthbean.debbie.javafx.PrimaryStage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.effect.Effect;
-import javafx.scene.effect.Reflection;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
