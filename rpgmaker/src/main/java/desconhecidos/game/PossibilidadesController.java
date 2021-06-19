cce8f348442deb4fd8b0c40bc38739b74dfe05fc
nathan <nathan.farias@dcx.ufpb.br>
2021-06-19 18:12:17 -0300

    rodando

--- rpgmaker/src/main/java/desconhecidos/game/PossibilidadesController.java
+++ rpgmaker/src/main/java/desconhecidos/game/PossibilidadesController.java
@@ -5,25 +5,22 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import Historia.Aventura;
-import Historia.Historia;
 import Historia.Possibilidade;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.TextFieldListCell;
 
 public class PossibilidadesController {
 
-    Historia historia = new Historia();
-    private ArrayList<Possibilidade> possibilidadesPreDefinidas = new ArrayList<>();
+    private ArrayList<PrePossibilidade> possibilidadesPreDefinidas = new ArrayList<>();
 
     private Possibilidade newNodeP;
 
-    @FXML
-    private SplitMenuButton MenuButtonId;
     
     private static MainGame mainGame;
     Aventura newAventura;
@@ -32,18 +29,22 @@ public class PossibilidadesController {
     private Button btnPrevious;
 
     @FXML 
-    private TextField textDescricao;
+    private TextArea textDescricao;
 
     @FXML
-    private TextField textOp1;
+    private TextArea textOp1;
 
     @FXML
-    private TextField textOp2;
+    private TextArea textOp2;
 
     @FXML
-    private TextField textOp3;
+    private TextArea textOp3;
+
     @FXML
     private Label labelApontador;
+
+    @FXML
+    private TextField tfPesquisaId;
 
     @FXML 
     protected void initialize(){
@@ -51,6 +52,7 @@ public class PossibilidadesController {
             @Override
             public void onScreenChanged(String newScreen, Object userData){
                 if(newScreen.equals("possibilidades")){
+                    tfPesquisaId.setText("");
                     System.out.println("Entrando no Frame Possibilidades...");
                     //AQUI DEVE SER IMPLEMENTADO A INTERAÇÃO COM A NOVA TELA 
                     newAventura = (Aventura) userData; 
@@ -62,49 +64,36 @@ public class PossibilidadesController {
     void concluirPossibilidade(ActionEvent event){
         if(!(textDescricao.getText().isEmpty() || textOp1.getText().isEmpty() || textOp2.getText().isEmpty() || textOp3.getText().isEmpty())){
             System.out.println("Preencher campos para continuar..");
-            if(Historia.possibilidades.size() == 0){
+            if(Aventura.possibilidades.size() == 0){
                 newNodeP = new Possibilidade(textDescricao.getText());
                 newNodeP.setApontador(null);
                 newNodeP.setMsgOp1(textOp1.getText());
                 newNodeP.setMsgOp2(textOp2.getText());
                 newNodeP.setMsgOp3(textOp3.getText());
                 newNodeP.setId("0");
-                Historia.addPossibilidade(newNodeP.getId(), newNodeP);
+                Aventura.addPossibilidade(newNodeP.getId(), newNodeP);
+                possibilidadesPreDefinidas.add(new PrePossibilidade(newNodeP.getId() + "1", newNodeP.getMsgOpcao1()));
+                possibilidadesPreDefinidas.add(new PrePossibilidade(newNodeP.getId() + "2", newNodeP.getMsgOpcao2())); 
+                possibilidadesPreDefinidas.add(new PrePossibilidade(newNodeP.getId() + "3", newNodeP.getMsgOpcao3()));
             }else{
-                //DEFINIR APONTADOR
-                newNodeP.setPossibilidade1();
-                Possibilidade newNode = newNodeP.getPossibilidade1();
-                newNodeP.setDescricao(textDescricao.getText());
-                newNodeP.setMsgOp1(textOp1.getText());
-                newNodeP.setMsgOp2(textOp2.getText());
-                newNodeP.setMsgOp3(textOp3.getText());
-                Historia.addPossibilidade(newNodeP.getId()+"1", newNode);
-
-
-
-                Possibilidade newP = new Possibilidade(textDescricao.getText());
-                Historia.updateId(newP);
-                newP.setMsgOp1(textOp1.getText());
-                newP.setMsgOp2(textOp2.getText());
-                newP.setMsgOp3(textOp3.getText());
-                Historia.addPossibilidade(newP.getId(), newP);
-
-                newP.setPossibilidade1();
-                newP.setPossibilidade2();
-                newP.setPossibilidade3();
+                if(!tfPesquisaId.getText().isEmpty()){
+                    for(PrePossibilidade pp : possibilidadesPreDefinidas){
+                        if(pp.getId().equals(tfPesquisaId.getText())){
+                            Possibilidade newP = new Possibilidade(textDescricao.getText());
+                            newP.setApontador(pp.getAPontador());
+                            newP.setMsgOp1(textOp1.getText());
+                            newP.setMsgOp2(textOp2.getText());
+                            newP.setMsgOp3(textOp3.getText());
+                            Aventura.addPossibilidade(newP.getId(), newP);
+                            possibilidadesPreDefinidas.remove(pp);
+                        }
+                    }
+                possibilidadesPreDefinidas.add(new PrePossibilidade (newNodeP.getId() + "1", newNodeP.getMsgOpcao1()));
+                possibilidadesPreDefinidas.add(new PrePossibilidade (newNodeP.getId() + "2", newNodeP.getMsgOpcao2())); 
+                possibilidadesPreDefinidas.add(new PrePossibilidade (newNodeP.getId() + "3", newNodeP.getMsgOpcao3()));
+                }
             }
         }
-    }
-    public void tratandoNode(String node){
-        
-
-
-    }
-    @FXML
-    void selecionandoNode(ActionEvent event){
-        //PODE SER CHAMADO PELO FRONT ENT NA CAIXINHA DE SELEÇÃO, DAÍ ELE PASSA QUAL É O ID DA POSSIBILIDADE
-        //precisa enviar pra 
-        tratandoNode("nome do menuItem");
     }
 
 
@@ -118,6 +107,9 @@ public class PossibilidadesController {
     }
     private void changeScreen(String scene){
         mainGame.changeScreen(scene);
+    }
+    public void updateTable(){
+        //ADICIONAR NOVAS POSSIBILIDADES
     }
 
     
---
+++ rpgmaker/src/main/java/desconhecidos/game/PrePossibilidade.java
@@ -0,0 +1,22 @@
+package desconhecidos.game;
+
+public class PrePossibilidade {
+    private String id;
+    private String apontador;
+
+    public PrePossibilidade(String id, String apontador){
+        this.id = id;
+        this.apontador = apontador;
+    }
+    public String getId(){
+        return this.id;
+    }
+    public String getAPontador(){
+        return this.apontador;
+    }public void setId(String id){
+        this.id = id;
+    }
+    public void setApontador(String apontador){
+        this.apontador = apontador;
+    }
+}
