bcd269de5a4cacc7e4a87aaac1ff5330e7e245fb
DenilsonSilvaMonteiroFilho <49108665+DenilsonSilvaMonteiroFilho@users.noreply.github.com>
2021-06-18 23:57:37 -0300

    test das opcoes

--- rpgmaker/src/main/java/Historia/Possibilidade.java
+++ rpgmaker/src/main/java/Historia/Possibilidade.java
@@ -1,7 +1,10 @@
 package Historia;
 
+import org.junit.jupiter.api.Test;
+
 public class Possibilidade {
-    private String mensagem;
+    private String Id;
+    private String mensagem,msgOpcao1,msgOpcao2,msgOpcao3;
     private Possibilidade opcao1,opcao2,opcao3;
 
 
@@ -17,6 +20,17 @@ public class Possibilidade {
     public void setMesagem(String msg){
         this.mensagem = msg;
     }
+    
+    public String getMsgOpcao1(){
+        return this.msgOpcao1;
+    }
+    public String getMsgOpcao2(){
+        return this.msgOpcao2;
+    }
+    public String getMsgOpcao3(){
+        return this.msgOpcao3;
+    }
+
     public Possibilidade getOpcao1(){
         return this.opcao1;
     }
@@ -26,6 +40,24 @@ public class Possibilidade {
     public Possibilidade getOpcao3(){
         return this.opcao3;
     }
+    public void setOpcao1(String msg){
+        opcao1 = new Possibilidade(msg);
+    }
+    public void setOpcao2(String msg){
+        opcao2 = new Possibilidade(msg);
+    }
+    public void setOpcao3(String msg){
+        opcao3 = new Possibilidade(msg);
+    }
 
+    @Test
+    public void test(){
+        Possibilidade p1 = new Possibilidade();
+        p1.setOpcao1("msg da opcao 1 do p1");
+        p1.getOpcao1().setOpcao1("msg da opcao 1 do opcao1");
+
+        System.out.println(p1.getOpcao1().getMensagem());
+        System.out.println(p1.getOpcao1().getOpcao1().getMensagem());
+    }
 
 }
